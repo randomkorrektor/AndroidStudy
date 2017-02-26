7bb79a868d425823d6d816feb39ad7ad0d1ff271
RanKor <randomkorrektor@gmail.com>
2017-02-26 20:13:12 +0700

    Lesson 5.3 Finish (don't work!)

---
+++ Sherlock/app/src/main/java/com/example/rankor/sherlock/ChoiceActivity.java
@@ -0,0 +1,37 @@
+package com.example.rankor.sherlock;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ChoiceActivity extends AppCompatActivity {
+
+    public final static String THIEF = "com.example.rankor.sherlock.THIEF";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+    }
+
+    public void onRadioClick(View view) {
+        Intent answerIntent = new Intent();
+        switch (view.getId()){
+            case R.id.radioButton1:
+                answerIntent.putExtra(THIEF, R.string.answer1);
+                break;
+            case R.id.radioButton2:
+                answerIntent.putExtra(THIEF,R.string.answer2);
+                break;
+            case R.id.radioButton3:
+                answerIntent.putExtra(THIEF,R.string.answer3);
+                default:
+                    answerIntent.putExtra(THIEF,"Fuck!");
+                    break;
+        }
+        setResult(RESULT_OK,answerIntent);
+        finish();
+    }
+}
---
+++ Sherlock/app/src/main/java/com/example/rankor/sherlock/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.rankor.sherlock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final private int CHOOSE_THIEF = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickChoice(View view) {
+        Intent questionIntent = new Intent(MainActivity.this, ChoiceActivity.class);
+        startActivityForResult(questionIntent, CHOOSE_THIEF);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        TextView infoTextView = (TextView)findViewById(R.id.textViewInfo);
+
+        if(requestCode == CHOOSE_THIEF){
+            String ans = data.getStringExtra(ChoiceActivity.THIEF);
+
+            infoTextView.setText(ans);
+        }else {
+            infoTextView.setText("");
+        }
+    }
+}
