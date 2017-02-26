eb3dd252b8a1624bafa9b0abbffce245a8a0aca5
RanKor <randomkorrektor@gmail.com>
2017-02-26 17:33:25 +0700

    Lesson 5.2 finished

--- AnotherScreen/app/src/main/java/com/example/rankor/anotherscreen/BirthdayActivity.java
+++ AnotherScreen/app/src/main/java/com/example/rankor/anotherscreen/BirthdayActivity.java
@@ -2,6 +2,7 @@ package com.example.rankor.anotherscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BirthdayActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class BirthdayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday);
+
+        String user = "Животное";
+        String message = "дырку от бублика";
+
+        if ((getIntent().getExtras().getString("username") != null)
+                && (getIntent().getExtras().getString("gift") != null)){
+            user = getIntent().getExtras().getString("username");
+            message = getIntent().getExtras().getString("gift");
+        }
+
+
+        TextView infoTextView = (TextView)findViewById(R.id.infoTextView);
+        infoTextView.setText(user + ", вам передали " + message);
     }
 }
--- AnotherScreen/app/src/main/java/com/example/rankor/anotherscreen/MainActivity.java
+++ AnotherScreen/app/src/main/java/com/example/rankor/anotherscreen/MainActivity.java
@@ -3,23 +3,47 @@ package com.example.rankor.anotherscreen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String name;
+    private EditText mEditTextName;
+    private EditText mEditTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditTextName = (EditText)findViewById(R.id.editTextName);
+        mEditTextMessage = (EditText)findViewById(R.id.editTextMessage);
+
+        mEditTextName.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if (event.getAction() == KeyEvent.ACTION_DOWN&&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    name = mEditTextName.getText().toString();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
         startActivity(intent);
+
     }
 
     public void onClickBirtday(View view) {
         Intent intent2 = new Intent(MainActivity.this, BirthdayActivity.class);
+        intent2.putExtra("username", mEditTextName.getText().toString());
+        intent2.putExtra("gift", mEditTextMessage.getText().toString());
         startActivity(intent2);
     }
 }
